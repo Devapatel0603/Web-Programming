4df3845634c16aee5cd416412aa15b599049899e
Dev Patel <142797423+Devapatel0603@users.noreply.github.com>
2024-05-28 11:50:46 +0530

    Update index.php

--- Practical-set-3-PHP/Practical-3/index.php
+++ Practical-set-3-PHP/Practical-3/index.php
@@ -1,2 +1,18 @@
 <!--Write PHP script to demonstrate use of Math functions. abs () & round (), ceil() & floor (), min () &
 max (), pow () & sqrt ().-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Math Function</title>
+ <link rel="stylesheet" href="style.css">
+</head>
+<body>
+ <div class="main">
+ <iframe src="left.php" class="size" frameborder="0"></iframe>
+ <iframe src="center.php" class="size" frameborder="0"></iframe>
+ <iframe src="right.php" class="size" frameborder="0"></iframe>
+</div>
+</body>
+</html>
